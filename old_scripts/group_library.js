791ff89c42af170502e83e6be33f5aff868cf0e2
Walter Benson <waltkbenson@gmail.com>
2019-08-13 14:30:04 -0500

    adding some comments

--- old_scripts/group_library.js
+++ old_scripts/group_library.js
@@ -2,6 +2,8 @@
 // Type: Trigger
 // Event: Creating an entry
 // Phase: Before saving an entry
+// Sets any empty group fields in the pallet library to the group letter of the current saved entry
+// Seems dangerous and unecessary looping. Also i've never seen the success message so I doubt it works.
 var e = entry();
 var g = e.field("group");
 
@@ -10,8 +12,7 @@ var pc = libByName("pallet").entries();
 for (var ent = 0; ent < pc.length; ent++) {
 	if (pc[ent].field("group")=== "") {
 		pc[ent].set("group",g);
-		message("success");} 
-		
+		message("success");} 	
 }
 
 // Title: group_duplication_update
